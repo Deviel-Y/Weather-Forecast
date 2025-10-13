fc8aec615c46a47edd9d2662f7482669d1318cbe
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-13 18:51:14 +0330

    Create CurrentDate component

---
+++ Weather Forecast/src/components/dashboardComponents/CurrentDate.tsx
@@ -0,0 +1,19 @@
+import { getCurrentDate } from "../../utils/getCurrentDate";
+
+const CurrentDate = () => {
+  const { date, time, weekday } = getCurrentDate();
+
+  return (
+    <div className="flex flex-col text-[#003464]">
+      <p className="font-[500] text-[32px]">{weekday}</p>
+
+      <div className="flex flex-row gap-x-5 font-sans text-sm">
+        <p>{date}</p>
+
+        <p>{time}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CurrentDate;
