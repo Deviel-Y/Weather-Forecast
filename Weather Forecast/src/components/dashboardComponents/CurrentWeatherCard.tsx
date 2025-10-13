c414ad5a3c578bcba6a20203607b79d932a214f2
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-14 01:16:46 +0330

    Be able to get city weather info as user selects city

--- Weather Forecast/src/components/dashboardComponents/CurrentWeatherCard.tsx
+++ Weather Forecast/src/components/dashboardComponents/CurrentWeatherCard.tsx
@@ -1,48 +1,56 @@
 import type { IconType } from "react-icons/lib";
-import LocationChip from "./LocationChip";
+import useCurrentWeather from "../../hooks/useCurrentWeather";
+import useCityQueryStore from "../../store";
 import CurrentDate from "./CurrentDate";
 import CurrentTempratureStatus from "./CurrentTempratureStatus";
 import CurrentWeatherSituaion from "./CurrentWeatherSituaion";
+import LocationChip from "./LocationChip";
 
 interface Props {
-  cityName: string;
-  weatherSituation: string;
-  Icon: IconType;
-  tempraure: {
-    currentTemp: string;
-    highTemp: string;
-    lowtemp: string;
-    feelsLikeTemp: string;
-  };
+ cityName: string;
+ weatherSituation: string;
+ Icon: IconType;
+ tempraure: {
+  currentTemp: string;
+  highTemp: string;
+  lowtemp: string;
+  feelsLikeTemp: string;
+ };
 }
 
 const CurrentWeatherCard = ({
-  Icon,
-  cityName,
-  tempraure,
-  weatherSituation,
+ Icon,
+ cityName,
+ tempraure,
+ weatherSituation,
 }: Props) => {
-  return (
-    <div className="flex items-center justify-center px-6 pt-5 pb-[17px] bg-[#E1E9EE] rounded-3xl shadow-[0_4px_10px_0_rgba(0,0,0,0.15)]">
-      <div className="flex flex-row justify-between w-full h-full ">
-        <div className="flex flex-col gap-2">
-          <LocationChip cityName={cityName} />
-          <CurrentDate />
-          <CurrentTempratureStatus
-            currentTemp={tempraure.currentTemp}
-            highTemp={tempraure.highTemp}
-            lowTemp={tempraure.lowtemp}
-          />
-        </div>
+ const { latitude, longitude } = useCityQueryStore(
+  (state) => state.cityAttrebutes
+ );
 
-        <CurrentWeatherSituaion
-          Icon={Icon}
-          feelsLike={tempraure.feelsLikeTemp}
-          weatherSituation={weatherSituation}
-        />
-      </div>
+ const {} = useCurrentWeather({ latitude, longitude });
+
+ return (
+  <div className="flex items-center justify-center px-6 pt-5 pb-[17px] bg-[#E1E9EE] rounded-3xl shadow-[0_4px_10px_0_rgba(0,0,0,0.15)]">
+   <div className="flex flex-row justify-between w-full h-full ">
+    <div className="flex flex-col gap-2">
+     <LocationChip cityName={cityName} />
+     <CurrentDate />
+     <CurrentTempratureStatus
+      currentTemp={tempraure.currentTemp}
+      highTemp={tempraure.highTemp}
+      lowTemp={tempraure.lowtemp}
+     />
     </div>
-  );
+
+    <CurrentWeatherSituaion
+     Icon={Icon}
+     feelsLike={tempraure.feelsLikeTemp}
+     weatherSituation={weatherSituation}
+    />
+   </div>
+  </div>
+ );
 };
 
 export default CurrentWeatherCard;
