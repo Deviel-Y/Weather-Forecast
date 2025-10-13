b0e4cb7b349e566700931dc78de46cb87e099fd5
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-13 11:05:31 +0330

    Create OptionPopoverButton component

---
+++ Weather Forecast/src/components/navbarComponents/OptionPopoverButton.tsx
@@ -0,0 +1,53 @@
+import { IconButton, Popover } from "@mui/material";
+import { useState, type ReactNode } from "react";
+import { HiOutlineCog6Tooth } from "react-icons/hi2";
+
+interface Props {
+  children: ReactNode;
+}
+
+const OptionPopoverButton = ({ children }: Props) => {
+  const [buttonElement, setButtonElement] = useState<HTMLButtonElement | null>(
+    null
+  );
+
+  const handlePopoverOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setButtonElement(event?.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setButtonElement(null);
+  };
+
+  const id = !!buttonElement ? "simple-popover" : undefined;
+
+  return (
+    <>
+      <IconButton
+        className="!border-[1px] !rounded-[8px] !border-[#BBC1C4]"
+        aria-describedby={id}
+        onClick={handlePopoverOpen}
+      >
+        <HiOutlineCog6Tooth />
+      </IconButton>
+      <Popover
+        open={!!buttonElement}
+        id={id}
+        onClose={handlePopoverClose}
+        anchorEl={buttonElement}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        {children}
+      </Popover>
+    </>
+  );
+};
+
+export default OptionPopoverButton;
