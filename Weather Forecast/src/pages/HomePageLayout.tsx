ff9ecb5fe4395aec5ee9483d2c77560651c2febf
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-13 18:53:01 +0330

    Add CurrentWeatherCard to dashboard

--- Weather Forecast/src/pages/HomePageLayout.tsx
+++ Weather Forecast/src/pages/HomePageLayout.tsx
@@ -1,12 +1,23 @@
-import { Outlet } from "react-router";
 import Navbar from "../components/navbarComponents/Navbar";
+import CurrentWeatherCard from "../components/dashboardComponents/CurrentWeatherCard";
+import { HiAdjustmentsHorizontal } from "react-icons/hi2";
 
 const HomePageLayout = () => {
   return (
-    <div className="h-screen w-full flex flex-col">
+    <div className="h-screen w-full flex flex-col gap-7 ">
       <Navbar />
 
-      <Outlet />
+      <CurrentWeatherCard
+        cityName="San Francisco"
+        Icon={HiAdjustmentsHorizontal}
+        tempraure={{
+          currentTemp: "2", //DummyData For Now!!
+          feelsLikeTemp: "2",
+          highTemp: "2",
+          lowtemp: "2",
+        }}
+        weatherSituation="cloudy"
+      />
     </div>
   );
 };
