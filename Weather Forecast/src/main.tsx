ccf95216f47945f1a220021458239f8203ca06ed
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-13 23:43:47 +0330

    Setup react query

--- Weather Forecast/src/main.tsx
+++ Weather Forecast/src/main.tsx
@@ -1,14 +1,19 @@
 import { ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import router from "./components/router";
 import theme from "./muiTheme";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
  <StrictMode>
-  <ThemeProvider theme={theme}>
-   <RouterProvider router={router} />
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+   <ThemeProvider theme={theme}>
+    <RouterProvider router={router} />
+   </ThemeProvider>
+  </QueryClientProvider>
  </StrictMode>
 );
