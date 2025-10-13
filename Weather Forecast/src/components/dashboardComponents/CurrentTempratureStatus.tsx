15cbbf9b56c7c11bbf4f7f4bc53e8a6802f5153a
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-13 18:51:26 +0330

    Create CurrentTempratureStatus component

---
+++ Weather Forecast/src/components/dashboardComponents/CurrentTempratureStatus.tsx
@@ -0,0 +1,25 @@
+interface TempratureProps {
+  currentTemp: string;
+  highTemp: string;
+  lowTemp: string;
+}
+
+const CurrentTempratureStatus = ({
+  currentTemp,
+  highTemp,
+  lowTemp,
+}: TempratureProps) => {
+  return (
+    <div className="flex flex-col text-[#003464]">
+      <p className="font-[500] text-[40px]">{currentTemp}&#8451;</p>
+
+      <div className="flex flex-row gap-x-1 font-sans text-sm -mt-2">
+        <p>High: {highTemp}</p>
+
+        <p>Low: {lowTemp}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CurrentTempratureStatus;
