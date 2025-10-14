57a31ff65d7b7c177590dbcc4f0fac579b28807d
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-14 17:47:30 +0330

    Add support for additional styles in CardContainer component

--- Weather Forecast/src/components/CardContainer.tsx
+++ Weather Forecast/src/components/CardContainer.tsx
@@ -2,11 +2,14 @@ import type { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  additionalStyles?: string;
 }
 
-const CardContainer = ({ children }: Props) => {
+const CardContainer = ({ children, additionalStyles }: Props) => {
   return (
-    <div className="flex items-center justify-center px-6 pt-5 pb-[17px] bg-[#E1E9EE] rounded-3xl shadow-[0_4px_10px_0_rgba(0,0,0,0.15)]">
+    <div
+      className={`${additionalStyles}  px-6 pt-5 pb-[17px] bg-[#E1E9EE] rounded-3xl shadow-[0_4px_10px_0_rgba(0,0,0,0.15)]`}
+    >
       {children}
     </div>
   );
