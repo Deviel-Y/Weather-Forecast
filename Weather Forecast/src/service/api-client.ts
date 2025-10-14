fa4144401355bc5f3558c58cbbbacf327f71cb7a
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-14 14:53:06 +0330

    Add more needed params to api client

--- Weather Forecast/src/service/api-client.ts
+++ Weather Forecast/src/service/api-client.ts
@@ -1,32 +1,32 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
- baseURL: "https://api.open-meteo.com/v1",
- params: {
-  daily: "temperature_2m_max,temperature_2m_min",
-  models: "icon_seamless",
-  current: "temperature_2m",
-  timezone: "auto",
-  forecast_days: 1,
- },
+  baseURL: "https://api.open-meteo.com/v1",
+  params: {
+    daily: "temperature_2m_max,temperature_2m_min",
+    models: "icon_seamless",
+    current: "apparent_temperature,temperature_2m,weather_code",
+    timezone: "auto",
+    forecast_days: 1,
+  },
 });
 
 class APIClient<T> {
- private latitude: number;
- private longitude: number;
+  private latitude: number;
+  private longitude: number;
 
- constructor(latitude: number, longitude: number) {
-  this.latitude = latitude;
-  this.longitude = longitude;
- }
+  constructor(latitude: number, longitude: number) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
 
- get = async () => {
-  const response = await axiosInstance.get<T>("/forecast", {
-   params: { latitude: this.latitude, longitude: this.longitude },
-  });
+  get = async () => {
+    const response = await axiosInstance.get<T>("/forecast", {
+      params: { latitude: this.latitude, longitude: this.longitude },
+    });
 
-  return response.data;
- };
+    return response.data;
+  };
 }
 
 export default APIClient;
