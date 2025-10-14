7b9e3248ff5b73f8be559435b65cddce6fec75e6
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-14 17:35:57 +0330

    Create MonthlyTemperatureChart component

---
+++ Weather Forecast/src/components/dashboardComponents/monthlyAverageChart/MonthlyAverageWheatherDataChart.tsx
@@ -0,0 +1,76 @@
+import { LineChart } from "@mui/x-charts/LineChart";
+
+interface Props {
+  data: {
+    month: string;
+    averageTemp: number;
+  }[];
+}
+
+function MonthlyTemperatureChart({ data }: Props) {
+  const temps = data.map((data) => data.averageTemp);
+  const months = data.map((data) => data.month);
+
+  return (
+    <div className="w-full bg-transparent h-full">
+      <svg width="0" height="0">
+        <defs>
+          <linearGradient id="tempGradient" x1="0" y1="0" x2="1" y2="0">
+            <stop offset="0%" stopColor="#4CDFE8" />
+            <stop offset="100%" stopColor="#7947F7" />
+          </linearGradient>
+        </defs>
+      </svg>
+
+      <LineChart
+        xAxis={[
+          {
+            data: months,
+            scaleType: "point",
+            tickLabelStyle: { fontSize: 12, fill: "#000" },
+            disableLine: true,
+          },
+        ]}
+        yAxis={[
+          {
+            min: Math.min(...temps) - 3,
+            max: Math.max(...temps) + 2,
+            labelStyle: { fontSize: 12, fill: "#000" },
+            tickLabelStyle: {
+              fontSize: 12,
+              fill: "#000",
+            },
+            disableLine: true,
+            valueFormatter: (value: number) => `${value}°C`,
+            offset: 30,
+          },
+        ]}
+        series={[
+          {
+            data: temps,
+            area: true,
+            color: "url(#tempGradient)",
+            curve: "linear",
+            showMark: false,
+          },
+        ]}
+        grid={{
+          horizontal: true,
+          vertical: false,
+        }}
+        sx={{
+          "& .MuiLineElement-root": { strokeWidth: 3 },
+          "& .MuiAreaElement-root": { fillOpacity: 0.05 },
+          "& .MuiChartsGrid-line": {
+            strokeDasharray: "5 5",
+            stroke: "#999",
+            opacity: 0.4,
+          },
+        }}
+        height={250}
+      />
+    </div>
+  );
+}
+
+export default MonthlyTemperatureChart;
