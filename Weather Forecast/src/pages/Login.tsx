3975a3c2fa7dc992a4401890c5f53643e0a3a7b8
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-14 13:00:35 +0330

    Prevent logged In user o visit login page

--- Weather Forecast/src/pages/Login.tsx
+++ Weather Forecast/src/pages/Login.tsx
@@ -1,15 +1,25 @@
+import { useNavigate } from "react-router";
 import LanguageSelectDropdown from "../components/loginComponents/LanguageSelectDropdown";
 import LoginForm from "../components/loginComponents/loginForm";
+import { useEffect } from "react";
 
 const LoginPage = () => {
- return (
-  <div className="w-full h-screen flex flex-row justify-center items-center bg-[#F3FAFE]">
-   <div className="w-full h-full flex flex-col items-center justify-center gap-5">
-    <LoginForm />
-    <LanguageSelectDropdown />
-   </div>
-  </div>
- );
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userName = localStorage.getItem("name");
+    
+    if (userName) navigate("/");
+  }, []);
+
+  return (
+    <div className="w-full h-screen flex flex-row justify-center items-center bg-[#F3FAFE]">
+      <div className="w-full h-full flex flex-col items-center justify-center gap-5">
+        <LoginForm />
+        <LanguageSelectDropdown />
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
