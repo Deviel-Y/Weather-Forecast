aafc743ee2099acf3608b63c6618b8d055105d48
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-14 14:55:02 +0330

    Narrow down the data that is needed for current weather

--- Weather Forecast/src/hooks/useCurrentWeather.ts
+++ Weather Forecast/src/hooks/useCurrentWeather.ts
@@ -2,21 +2,36 @@ import { useQuery } from "@tanstack/react-query";
 import APIClient from "../service/api-client";
 
 interface GetWeatherParams {
- latitude: number | null;
- longitude: number | null;
+  latitude: number | null;
+  longitude: number | null;
+}
+
+export interface CurrentWeatherResponseType {
+  current: {
+    temperature_2m: number;
+    apparent_temperature: number;
+    weather_code: number;
+  };
+  daily: {
+    temperature_2m_max: [number];
+    temperature_2m_min: [number];
+  };
 }
 
 const useCurrentWeather = ({ latitude, longitude }: GetWeatherParams) => {
- return useQuery({
-  queryKey: [latitude, longitude],
-  queryFn: () => {
-   const apiClient = new APIClient(latitude!, longitude!);
+  return useQuery({
+    queryKey: [latitude, longitude],
+    queryFn: () => {
+      const apiClient = new APIClient<CurrentWeatherResponseType>(
+        latitude!,
+        longitude!
+      );
 
-   return apiClient.get();
-  },
+      return apiClient.get();
+    },
 
-  enabled: latitude !== null && longitude !== null,
- });
+    enabled: latitude !== null && longitude !== null,
+  });
 };
 
 export default useCurrentWeather;
