98add270a60f102f033cad0527e21b73c891650e
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-14 21:47:29 +0330

    Add dateInDashFormat & currentYear to getCurrentDate function

--- Weather Forecast/src/utils/getCurrentDate.ts
+++ Weather Forecast/src/utils/getCurrentDate.ts
@@ -1,19 +1,25 @@
 export const getCurrentDate = () => {
-  const now = new Date();
+ const now = new Date();
 
-  const weekday = now.toLocaleString("en-US", {
-    weekday: "long",
-  });
+ const weekday = now.toLocaleString("en-US", {
+  weekday: "long",
+ });
 
-  const time = now.toLocaleString("en-US", {
-    hour: "numeric",
-    minute: "2-digit",
-    hour12: true,
-  });
+ const time = now.toLocaleString("en-US", {
+  hour: "numeric",
+  minute: "2-digit",
+  hour12: true,
+ });
 
-  const date = `${now.getDate()} ${now.toLocaleString("en-US", {
-    month: "short",
-  })}, ${now.getFullYear()}`;
+ const date = `${now.getDate()} ${now.toLocaleString("en-US", {
+  month: "short",
+ })}, ${now.getFullYear()}`;
 
-  return { weekday, time, date };
+ const dateInDashFormat = `${now.getFullYear()}-${
+  now.getMonth() + 1
+ }-${now.getDate()}`;
+
+ const currentYear = now.getFullYear();
+
+ return { weekday, time, date, dateInDashFormat, currentYear };
 };
