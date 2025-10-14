73a78817475faa830b384e553fe07d3239e3000e
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-14 21:47:38 +0330

    Add API client for monthly weather data retrieval

---
+++ Weather Forecast/src/service/api-client-monthlyData.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { getCurrentDate } from "../utils/getCurrentDate";
+
+const { dateInDashFormat, currentYear } = getCurrentDate();
+
+const axiosInstance = axios.create({
+ baseURL: "https://historical-forecast-api.open-meteo.com/v1",
+ params: {
+  daily: "temperature_2m_mean",
+  timezone: "auto",
+  start_date: `${currentYear}-01-01`,
+  end_date: dateInDashFormat,
+ },
+});
+
+class APIClientMonthlyData<T> {
+ private latitude: number;
+ private longitude: number;
+
+ constructor(latitude: number, longitude: number) {
+  this.latitude = latitude;
+  this.longitude = longitude;
+ }
+
+ getMonthlyData = async () => {
+  const response = await axiosInstance.get<T>("/forecast", {
+   params: { latitude: this.latitude, longitude: this.longitude },
+  });
+
+  return response.data;
+ };
+}
+
+export default APIClientMonthlyData;
