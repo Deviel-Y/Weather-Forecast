c4da089246742b99ddd24ab687eee9323291ab10
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-15 23:44:16 +0330

    Fix layout issue of each weather card

--- Weather Forecast/src/components/dashboardComponents/weeklyWeatcher/SingleWeatherCard.tsx
+++ Weather Forecast/src/components/dashboardComponents/weeklyWeatcher/SingleWeatherCard.tsx
@@ -1,30 +1,33 @@
 interface Props {
-  date: string;
-  figure: string;
-  temperature: number;
+ date: string;
+ figure: string;
+ temperature: number;
 }
 
 const SingleWeatherCard = ({ date, figure, temperature }: Props) => {
-  return (
-    <div className="bg-[#CDD9E0] rounded-3xl px-4 py-11 w-[104px]">
-      <div className="flex flex-col gap-[26px] ">
-        <div className="flex flex-col gap-4 justify-between items-center">
-          <p className="font-[500] text-sm text-[#003464]">{date}</p>
-          <div className="w-[60px] h-[2px] rounded-full bg-gradient-to-r from-[#36363600] via-[#7E7E7E] to-[#36363600]" />
-        </div>
-
-        <img
-          className="object-contain w-[72px] h-[72]"
-          src={figure}
-          alt="Weather figure"
-        />
-
-        <p className="font-[500] text-lg text-center text-[#003464]">
-          {temperature}&#8451;
-        </p>
-      </div>
+ return (
+  <div className="bg-[#CDD9E0] rounded-3xl pt-11 px-4 w-[104px] h-[266px]">
+   <div className="flex flex-col gap-[26px] ">
+    <div className="flex flex-col gap-4 justify-between items-center">
+     <p className="font-[500] text-sm text-[#003464]">{date}</p>
+     <div className="w-[60px] h-[2px] rounded-full bg-gradient-to-r from-[#36363600] via-[#7E7E7E] to-[#36363600]" />
     </div>
-  );
+
+    <div className="w-[72px] h-[72px] flex justify-center items-center">
+     <img
+      draggable={false}
+      className="w-full h-full object-contain select-none"
+      src={figure}
+      alt="Weather figure"
+     />
+    </div>
+
+    <p className="font-[500] text-lg text-center text-[#003464]">
+     {temperature}&#8451;
+    </p>
+   </div>
+  </div>
+ );
 };
 
 export default SingleWeatherCard;
