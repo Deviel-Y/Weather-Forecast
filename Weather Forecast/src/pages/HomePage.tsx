813a655f43f858d3afc7273f300f8e2fa3adb61c
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-15 11:35:15 +0330

    Show weekly weather data placeholder in home page

--- Weather Forecast/src/pages/HomePage.tsx
+++ Weather Forecast/src/pages/HomePage.tsx
@@ -6,71 +6,106 @@ import useCurrentWeather from "../hooks/useWeatherData";
 import useCityQueryStore from "../store";
 import { getMonthlyAverageTemps } from "../utils/getMonthlyAverageTemps";
 import {
- weatherCodeMapping,
- type WeatherCodeType,
+  weatherCodeMapping,
+  type WeatherCodeType,
 } from "../utils/weatherCodeMapping";
+import SingleWeatherCard from "../components/dashboardComponents/weeklyWeatcher/SingleWeatherCard.tsx";
 
 const HomePage = () => {
- const MonthlyTemperatureChart = lazy(
-  () =>
-   import(
-    "../components/dashboardComponents/monthlyAverageChart/MonthlyAverageWheatherDataChart.tsx"
-   )
- );
- const { latitude, longitude, name } = useCityQueryStore(
-  (state) => state.cityAttrebutes
- );
+  const MonthlyTemperatureChart = lazy(
+    () =>
+      import(
+        "../components/dashboardComponents/monthlyAverageChart/MonthlyAverageWheatherDataChart.tsx"
+      )
+  );
 
- const { currentWeather, monthlyWeather } = useCurrentWeather({
-  latitude,
-  longitude,
- });
+  const { latitude, longitude, name } = useCityQueryStore(
+    (state) => state.cityAttrebutes
+  );
 
- const monthlyAverageChartData = getMonthlyAverageTemps(
-  monthlyWeather?.data ?? { daily: { temperature_2m_mean: [], time: [] } }
- );
+  const { currentWeather, monthlyWeather } = useCurrentWeather({
+    latitude,
+    longitude,
+  });
 
- return (
-  <div className="h-screen w-full flex flex-col">
-   <Navbar />
+  const monthlyAverageChartData = getMonthlyAverageTemps(
+    monthlyWeather?.data ?? { daily: { temperature_2m_mean: [], time: [] } }
+  );
 
-   <div className="grid grid-cols-12 w-full gap-10 px-6 py-7">
-    <CardContainer additionalStyles="flex flex-1 items-center justify-center col-span-5 px-6 pt-5 pb-[17px] shadow-[0_4px_10px_0_rgba(0,0,0,0.15)]">
-     <CurrentWeatherCard
-      cityName={name}
-      weatherFigure={
-       weatherCodeMapping[
-        currentWeather?.data?.current?.weather_code as WeatherCodeType
-       ]?.figure
-      }
-      tempraure={{
-       currentTemp: currentWeather.data?.current.temperature_2m ?? 0,
-       feelsLikeTemp: currentWeather.data?.current.apparent_temperature ?? 0,
-       highTemp: currentWeather.data?.daily.temperature_2m_max[0] ?? 0,
-       lowtemp: currentWeather.data?.daily.temperature_2m_min[0] ?? 0,
-      }}
-      weatherSituation={
-       weatherCodeMapping[
-        currentWeather.data?.current.weather_code as WeatherCodeType
-       ]?.label
-      }
-     />
-    </CardContainer>
+  return (
+    <div className="h-screen w-full flex flex-col">
+      <Navbar />
 
-    <CardContainer additionalStyles="flex flex-col gap-1 col-span-7 p-4">
-     <p className="font-bold text-lg font-sans text-[#1B2767]">
-      Average Monthly Temprature
-     </p>
+      <div className="px-6 py-7 flex flex-col gap-6">
+        <div className="grid grid-cols-12 w-full gap-10">
+          <CardContainer additionalStyles="flex flex-1 items-center justify-center col-span-5 px-6 pt-5 pb-[17px] shadow-[0_4px_10px_0_rgba(0,0,0,0.15)]">
+            <CurrentWeatherCard
+              cityName={name}
+              weatherFigure={
+                weatherCodeMapping[
+                  currentWeather?.data?.current?.weather_code as WeatherCodeType
+                ]?.figure
+              }
+              tempraure={{
+                currentTemp: currentWeather.data?.current.temperature_2m ?? 0,
+                feelsLikeTemp:
+                  currentWeather.data?.current.apparent_temperature ?? 0,
+                highTemp: currentWeather.data?.daily.temperature_2m_max[0] ?? 0,
+                lowtemp: currentWeather.data?.daily.temperature_2m_min[0] ?? 0,
+              }}
+              weatherSituation={
+                weatherCodeMapping[
+                  currentWeather.data?.current.weather_code as WeatherCodeType
+                ]?.label
+              }
+            />
+          </CardContainer>
 
-     <Suspense fallback={<p>Loading Chart...</p>}>
-      <div className="w-full ps-8">
-       <MonthlyTemperatureChart data={monthlyAverageChartData} />
+          <CardContainer additionalStyles="flex flex-col gap-1 col-span-7 p-4">
+            <p className="font-bold text-lg font-sans text-[#1B2767]">
+              Average Monthly Temprature
+            </p>
+
+            <Suspense fallback={<p>Loading Chart...</p>}>
+              <div className="w-full ps-8">
+                <MonthlyTemperatureChart data={monthlyAverageChartData} />
+              </div>
+            </Suspense>
+          </CardContainer>
+        </div>
+
+        <CardContainer additionalStyles="flex flex-col px-[28px] pb-[26px] pt-[30px] gap-5 items-center justify-center">
+          <p className="font-bold font-sans self-start text-[#1B2767] text-2xl">
+            2 weeks Forecast
+          </p>
+
+          <div
+            className="
+              flex flex-row gap-[18px]
+              overflow-scroll no-scrollbar
+              w-full snap-x snap-mandatory
+              cursor-grab active:cursor-grabbing select-none
+            "
+          >
+            {[...Array(14)].map((_, index) => (
+              <div key={index} className="snap-start shrink-0">
+                <SingleWeatherCard
+                  date={index.toString()}
+                  figure={
+                    weatherCodeMapping[
+                      currentWeather.data?.current
+                        .weather_code as WeatherCodeType
+                    ]?.figure
+                  }
+                  temperature={20}
+                />
+              </div>
+            ))}
+          </div>
+        </CardContainer>
       </div>
-     </Suspense>
-    </CardContainer>
-   </div>
-  </div>
- );
+    </div>
+  );
 };
 
 export default HomePage;
