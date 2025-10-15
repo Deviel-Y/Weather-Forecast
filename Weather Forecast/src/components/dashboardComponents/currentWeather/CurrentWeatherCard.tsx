70a350ec76c07f6e812abf28972a50c4e524c166
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-16 01:40:16 +0330

    Refactor: Wrap home component inside CardContainer

--- Weather Forecast/src/components/dashboardComponents/currentWeather/CurrentWeatherCard.tsx
+++ Weather Forecast/src/components/dashboardComponents/currentWeather/CurrentWeatherCard.tsx
@@ -1,3 +1,4 @@
+import CardContainer from "../../CardContainer";
 import CurrentDate from "./CurrentDate";
 import CurrentTempratureStatus from "./CurrentTempratureStatus";
 import CurrentWeatherSituaion from "./CurrentWeatherSituaion";
@@ -22,25 +23,27 @@ const CurrentWeatherCard = ({
  weatherSituation,
 }: Props) => {
  return (
-  <div className="flex flex-row justify-between w-full h-full ">
-   <div className="flex flex-col gap-2">
-    <LocationChip cityName={cityName} />
-    <CurrentDate />
-    <CurrentTempratureStatus
-     currentTemp={tempraure.currentTemp}
-     highTemp={tempraure.highTemp}
-     lowTemp={tempraure.lowtemp}
-    />
-   </div>
+  <CardContainer additionalStyles="flex flex-1 items-center justify-center col-span-5 px-6 pt-5 pb-[17px] max-md:p-5 shadow-[0_4px_10px_0_rgba(0,0,0,0.15)]">
+   <div className="flex flex-row justify-between w-full h-full ">
+    <div className="flex flex-col gap-2">
+     <LocationChip cityName={cityName} />
+     <CurrentDate />
+     <CurrentTempratureStatus
+      currentTemp={tempraure.currentTemp}
+      highTemp={tempraure.highTemp}
+      lowTemp={tempraure.lowtemp}
+     />
+    </div>
 
-   <div className="w-fit pe-9">
-    <CurrentWeatherSituaion
-     weatherFigure={weatherFigure}
-     feelsLike={tempraure.feelsLikeTemp}
-     weatherSituation={weatherSituation}
-    />
+    <div className="w-fit pe-9">
+     <CurrentWeatherSituaion
+      weatherFigure={weatherFigure}
+      feelsLike={tempraure.feelsLikeTemp}
+      weatherSituation={weatherSituation}
+     />
+    </div>
    </div>
-  </div>
+  </CardContainer>
  );
 };
 
--- Weather Forecast/src/components/dashboardComponents/monthlyAverageChart/MonthlyAverageWheatherDataChart.tsx
+++ Weather Forecast/src/components/dashboardComponents/monthlyAverageChart/MonthlyAverageWheatherDataChart.tsx
@@ -1,5 +1,6 @@
 import { LineChart } from "@mui/x-charts/LineChart";
 import type { MonthlyAverage } from "../../../utils/getMonthlyAverageTemps";
+import CardContainer from "../../CardContainer";
 
 interface Props {
  data: MonthlyAverage[];
@@ -10,64 +11,70 @@ function MonthlyTemperatureChart({ data }: Props) {
  const months = data.map((data) => data.monthName);
 
  return (
-  <div className="w-full bg-transparent h-full">
-   <svg width="0" height="0">
-    <defs>
-     <linearGradient id="tempGradient" x1="0" y1="0" x2="1" y2="0">
-      <stop offset="0%" stopColor="#4CDFE8" />
-      <stop offset="100%" stopColor="#7947F7" />
-     </linearGradient>
-    </defs>
-   </svg>
+  <CardContainer additionalStyles="flex flex-col gap-1 col-span-7 p-4 max-sm:p-5 w-full ps-8 max-sm:p-1">
+   <div className="w-full bg-transparent h-full">
+    <p className="font-bold text-lg font-sans text-[#1B2767]">
+     Average Monthly Temprature
+    </p>
 
-   <LineChart
-    xAxis={[
-     {
-      data: months,
-      scaleType: "point",
-      tickLabelStyle: { fontSize: 12, fill: "#000" },
-      disableLine: true,
-     },
-    ]}
-    yAxis={[
-     {
-      min: Math.min(...temps) - 3,
-      max: Math.max(...temps) + 2,
-      labelStyle: { fontSize: 12, fill: "#000" },
-      tickLabelStyle: {
-       fontSize: 12,
-       fill: "#000",
+    <svg width="0" height="0">
+     <defs>
+      <linearGradient id="tempGradient" x1="0" y1="0" x2="1" y2="0">
+       <stop offset="0%" stopColor="#4CDFE8" />
+       <stop offset="100%" stopColor="#7947F7" />
+      </linearGradient>
+     </defs>
+    </svg>
+
+    <LineChart
+     xAxis={[
+      {
+       data: months,
+       scaleType: "point",
+       tickLabelStyle: { fontSize: 12, fill: "#000" },
+       disableLine: true,
       },
-      disableLine: true,
-      valueFormatter: (value: number) => `${value}°C`,
-      offset: 30,
-     },
-    ]}
-    series={[
-     {
-      data: temps,
-      area: true,
-      color: "url(#tempGradient)",
-      curve: "linear",
-      showMark: false,
-     },
-    ]}
-    grid={{
-     horizontal: true,
-     vertical: false,
-    }}
-    sx={{
-     "& .MuiLineElement-root": { strokeWidth: 3 },
-     "& .MuiAreaElement-root": { fillOpacity: 0.05 },
-     "& .MuiChartsGrid-line": {
-      strokeDasharray: "5 5",
-      stroke: "#999",
-      opacity: 0.4,
-     },
-    }}
-    height={170}
-   />
-  </div>
+     ]}
+     yAxis={[
+      {
+       min: Math.min(...temps) - 3,
+       max: Math.max(...temps) + 2,
+       labelStyle: { fontSize: 12, fill: "#000" },
+       tickLabelStyle: {
+        fontSize: 12,
+        fill: "#000",
+       },
+       disableLine: true,
+       valueFormatter: (value: number) => `${value}°C`,
+       offset: 30,
+      },
+     ]}
+     series={[
+      {
+       data: temps,
+       area: true,
+       color: "url(#tempGradient)",
+       curve: "linear",
+       showMark: false,
+      },
+     ]}
+     grid={{
+      horizontal: true,
+      vertical: false,
+     }}
+     sx={{
+      "& .MuiLineElement-root": { strokeWidth: 3 },
+      "& .MuiAreaElement-root": { fillOpacity: 0.05 },
+      "& .MuiChartsGrid-line": {
+       strokeDasharray: "5 5",
+       stroke: "#999",
+       opacity: 0.4,
+      },
+     }}
+     height={170}
+    />
+   </div>
+  </CardContainer>
  );
 }
 
