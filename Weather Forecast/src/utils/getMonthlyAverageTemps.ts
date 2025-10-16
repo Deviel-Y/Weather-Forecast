1b91464a222c7be40ed7b6df9ba952ecb4365aea
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-16 22:00:00 +0330

    Refactor: Simplify getMonthlyAverageTemps function and enhance month index handling

--- Weather Forecast/src/utils/getMonthlyAverageTemps.ts
+++ Weather Forecast/src/utils/getMonthlyAverageTemps.ts
@@ -1,45 +1,40 @@
+import moment from "moment-jalaali";
 import type { WeatherDataType } from "../hooks/useWeatherData";
+import type { LanguageType } from "../i18n";
 
 export interface MonthlyAverage {
  monthName: string;
  averageTemp: number;
 }
 
-export const getMonthlyAverageTemps = ({
- daily: { temperature_2m_mean, time },
-}: WeatherDataType): MonthlyAverage[] => {
+export const getMonthlyAverageTemps = (
+ data: WeatherDataType,
+ translatedMonthNames: string[],
+ lang: LanguageType
+): MonthlyAverage[] => {
  const monthlyMapping = new Map<number, number[]>();
 
- time.forEach((timeData, index) => {
-  const monthName = new Date(timeData).getMonth();
-  const temperature = temperature_2m_mean[index];
+ data.daily.time.forEach((timeData, index) => {
+  let monthIndex: number;
+  if (lang === "fa") {
+   const jMonth = moment(timeData).jMonth(); // 0–11
+   monthIndex = jMonth;
+  } else {
+   monthIndex = new Date(timeData).getMonth(); // 0–11
+  }
 
-  if (!monthlyMapping.has(monthName)) monthlyMapping.set(monthName, []);
-  monthlyMapping.get(monthName)?.push(temperature);
+  const temperature = data.daily.temperature_2m_mean[index];
+  if (!monthlyMapping.has(monthIndex)) monthlyMapping.set(monthIndex, []);
+  monthlyMapping.get(monthIndex)?.push(temperature);
  });
 
- return Array.from(monthlyMapping).map(([monthIndex, temperature]) => {
+ return Array.from(monthlyMapping).map(([monthIndex, temperatures]) => {
   const averageTemp =
-   temperature.reduce((acc, sum) => acc + sum, 0) / temperature.length || 1;
+   temperatures.reduce((acc, sum) => acc + sum, 0) / temperatures.length || 1;
 
   return {
-   monthName: monthNames[monthIndex],
+   monthName: translatedMonthNames[monthIndex],
    averageTemp: parseFloat(averageTemp.toFixed(1)),
   };
  });
 };
-
-const monthNames = [
- "January",
- "February",
- "March",
- "April",
- "May",
- "June",
- "July",
- "August",
- "September",
- "October",
- "November",
- "December",
-];
