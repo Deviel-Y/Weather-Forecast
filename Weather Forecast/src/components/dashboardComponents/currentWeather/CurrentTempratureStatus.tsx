db76709f1fda21bddc66241a876b09bcc5f8894a
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-16 21:15:55 +0330

    Refactor: Improve temperature display logic and add translation for temperature labels

--- Weather Forecast/src/components/dashboardComponents/currentWeather/CurrentTempratureStatus.tsx
+++ Weather Forecast/src/components/dashboardComponents/currentWeather/CurrentTempratureStatus.tsx
@@ -1,25 +1,41 @@
+import { useTranslation } from "react-i18next";
+import useWeatherinfoStore from "../../../useLanguageStore";
+import { toPersianDigits } from "../../../utils/changeNumberToPersian";
+
 interface TempratureProps {
-  currentTemp: number;
-  highTemp: number;
-  lowTemp: number;
+ currentTemp: number;
+ highTemp: number;
+ lowTemp: number;
 }
 
 const CurrentTempratureStatus = ({
-  currentTemp,
-  highTemp,
-  lowTemp,
+ currentTemp,
+ highTemp,
+ lowTemp,
 }: TempratureProps) => {
-  return (
-    <div className="flex flex-col text-[#003464]">
-      <p className="font-[500] text-[40px]">{currentTemp}&#8451;</p>
+ const { t } = useTranslation();
+ const currentLang = useWeatherinfoStore((s) => s.currentLang);
+ return (
+  <div className="flex flex-col text-[#003464]">
+   <p className="font-[500] text-[40px]">
+    {currentLang === "en" ? currentTemp : toPersianDigits(currentTemp)}&#8451;
+   </p>
 
-      <div className="flex flex-row gap-x-1 font-sans text-sm -mt-2">
-        <p>High: {highTemp}</p>
+   <div className="flex flex-row gap-x-1 font-sans text-sm -mt-2">
+    <div className="w-fit flex gap-1 flex-row items-center">
+     <p>{t("highTemp")}: </p>
 
-        <p>Low: {lowTemp}</p>
-      </div>
+     <p>{currentLang === "en" ? highTemp : toPersianDigits(highTemp)}</p>
     </div>
-  );
+
+    <div className="w-fit flex gap-1 flex-row items-center">
+     <p>{t("lowTemp")}:</p>
+
+     <p>{currentLang === "en" ? lowTemp : toPersianDigits(lowTemp)}</p>
+    </div>
+   </div>
+  </div>
+ );
 };
 
 export default CurrentTempratureStatus;
