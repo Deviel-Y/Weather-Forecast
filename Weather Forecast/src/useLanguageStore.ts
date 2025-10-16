5faddecb2a485599cf89a294db0f9a6091d34fff
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-17 02:42:24 +0330

    Add dir and setDir  to zustand language store

--- Weather Forecast/src/useLanguageStore.ts
+++ Weather Forecast/src/useLanguageStore.ts
@@ -1,14 +1,22 @@
 import { create } from "zustand";
 import type { LanguageType } from "./i18n";
 
+export type DirType = "rtl" | "ltr";
+
 interface LanguageStoreType {
  currentLang: LanguageType;
  setCurrentLang: (lang: LanguageType) => void;
+
+ dir: DirType;
+ setDir: (dir: DirType) => void;
 }
 
 const useLanguageStore = create<LanguageStoreType>((set) => ({
  currentLang: localStorage.getItem("lang") as LanguageType,
  setCurrentLang: (lang) => set(() => ({ currentLang: lang })),
+
+ dir: localStorage.getItem("dir") as DirType,
+ setDir: (dir) => set(() => ({ dir })),
 }));
 
 export default useLanguageStore;
