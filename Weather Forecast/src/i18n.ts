41a5961c0179ed46f7d1911e274b1e6e9e2e858c
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-16 20:54:46 +0330

    Fix: Set default language to localStorage value for i18n configuration

--- Weather Forecast/src/i18n.ts
+++ Weather Forecast/src/i18n.ts
@@ -3,6 +3,8 @@ import { initReactI18next } from "react-i18next";
 import enTranslation from "./locales/english.json";
 import faTranslation from "./locales/persian.json";
 
+export type LanguageType = "fa" | "en";
+
 i18n
  .use(initReactI18next) // initializes i18next with react-i18next
  .init({
@@ -10,7 +12,7 @@ i18n
    en: { translation: enTranslation },
    fa: { translation: faTranslation },
   }, // feel free to add any other you want!
-  lng: "en", // default language
+  lng: localStorage.getItem("lang") as LanguageType, // default language
   interpolation: {
    escapeValue: false, // This ensures raw HTML in translations (if needed)
   },
