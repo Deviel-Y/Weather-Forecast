599ad4394ca628f453c6e7620518cbc073789254
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-16 21:07:23 +0330

    Change date format as user changes the language

--- Weather Forecast/src/components/dashboardComponents/currentWeather/CurrentDate.tsx
+++ Weather Forecast/src/components/dashboardComponents/currentWeather/CurrentDate.tsx
@@ -1,19 +1,28 @@
+import useLanguageStore from "../../../useLanguageStore";
 import { getCurrentDate } from "../../../utils/getCurrentDate";
 
 const CurrentDate = () => {
-  const { date, time, weekday } = getCurrentDate();
+ const currentLang = useLanguageStore((s) => s.currentLang);
+ const { gregorian, jalali } = getCurrentDate();
 
-  return (
-    <div className="flex flex-col text-[#003464]">
-      <p className="font-[500] text-[32px]">{weekday}</p>
+ const gregorianDateFormat = `${gregorian.dayOfTheMonth} ${gregorian.month}, ${gregorian.year}`;
+ const jalaliDateFormat = `${jalali.weekday} ${jalali.day} ${jalali.month} ${jalali.year}`;
 
-      <div className="flex flex-row gap-x-5 font-sans text-sm">
-        <p>{date}</p>
+ return (
+  <div className="flex flex-col text-[#003464]">
+   <p className="font-[500] text-[32px]">
+    {currentLang === "en" ? gregorian.weekday : jalali.weekday}
+   </p>
 
-        <p>{time}</p>
-      </div>
-    </div>
-  );
+   <div className="flex flex-row gap-x-5 font-sans text-sm">
+    <p>{currentLang === "en" ? gregorianDateFormat : jalaliDateFormat}</p>
+
+    <p>
+     {currentLang === "en" ? gregorian.time : jalali.jalaliTimeWithMeridiem}
+    </p>
+   </div>
+  </div>
+ );
 };
 
 export default CurrentDate;
