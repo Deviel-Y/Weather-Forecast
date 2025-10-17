abc46b637bf83235e422ed73bf0b72030804ec28
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-17 03:36:55 +0330

    Translate footer text

--- Weather Forecast/src/components/footerComponents/Footer.tsx
+++ Weather Forecast/src/components/footerComponents/Footer.tsx
@@ -1,26 +1,30 @@
+import { useTranslation } from "react-i18next";
 import { FaRegEnvelope } from "react-icons/fa6";
 import { MdOutlineCalendarMonth } from "react-icons/md";
 import compnayLogo from "../../assets/companyLogo.png";
+import useLanguageStore from "../../useLanguageStore";
 import { getCurrentDate } from "../../utils/getCurrentDate";
 
 const Footer = () => {
- const { gregorian } = getCurrentDate();
+ const { gregorian, jalali } = getCurrentDate();
+ const { t } = useTranslation();
+ const dir = useLanguageStore((s) => s.dir);
  return (
   <footer className="flex flex-row items-center justify-between px-6 h-[106px] bg-gradient-to-r from-[#F3FAFE] via-[#CCDDDD9E] to-[#F3FAFE]">
    <div className="flex flex-row items-center gap-3">
     <img src={compnayLogo} alt="company Logo" />
 
-    <p className="text-lg text-[#003464]">
-     All rights of this site are reserved for Nadin Sadr Aria Engineering
-     Company.
-    </p>
+    <p className="text-lg text-[#003464] rtl:text-sm">{t("footerCopyRight")}</p>
    </div>
 
    <div className="flex flex-row items-center gap-10 text-sm">
     <div className="flex flex-row items-center gap-3">
      <FaRegEnvelope size={19} />
 
-     <p>contact us : info@nadin.ir</p>
+     <p className="flex flex-row items-center gap-1">
+      <span>{t("contactUs")} : </span>
+      <span>info@nadin.ir</span>
+     </p>
     </div>
 
     <div className="flex flex-row items-center gap-3">
@@ -28,9 +32,12 @@ const Footer = () => {
 
      <div>
       <span>{`${gregorian.gregoryTimeIn24} . `}</span>
-      <span>{gregorian.weekday} </span>
-      <span>{gregorian.dayOfTheMonth} </span>
-      <span>{gregorian.year} </span>
+      <span>{dir === "ltr" ? gregorian.weekday : jalali.weekday} </span>
+      <span>{dir === "ltr" ? gregorian.dayOfTheMonth : jalali.day} </span>
+      <span>
+       {dir === "ltr" ? gregorian.gregoryMonthInLong : jalali.month}{" "}
+      </span>
+      <span>{dir === "ltr" ? gregorian.year : jalali.year} </span>
      </div>
     </div>
    </div>
